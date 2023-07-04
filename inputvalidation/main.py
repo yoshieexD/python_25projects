8864d4b4a4c24366a4dae062a5dfd224360e1383
Jom-Winston <bjomwinston0107@gmail.com>
2023-07-04 10:20:17 +0800

    not done

--- inputvalidation/main.py
+++ inputvalidation/main.py
@@ -1,8 +1,15 @@
-name = input('Enter your name  ')
+import re
+name = input("Enter your name:\n")
 
-if type(input) is str:
-    print("You entered right")
-elif type(input) is int or float:
-    print("You entered wrong")
+if re.match('[0-9]', name): 
+  print("Sorry enter a proper name")
+else:
+  username = input("Enter your username:\n")
+  password = input("Enter your password:\n")
+  confirmpassword = input("enter your password again:\n")
+  if password  == confirmpassword:
+    print('your password matched')
+  else:
+    print('Enter your password again')
 
 
